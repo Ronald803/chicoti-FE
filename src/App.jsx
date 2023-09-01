7eaa451d0205aa04facce497e773a7a6f5182c2a
Ronald803 <ronaldblancobalboa@gmail.com>
2023-08-31 23:50:24 -0400

    navbar and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import HomePage from './pages/HomePage'
 import MissingAnimalsPage from './pages/MissingAnimalsPage'
 import FoundAnimalsPage from './pages/FoundAnimalsPage'
 import HomelessAnimalsPage from './pages/HomelessAnimalsPage'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 
 
@@ -20,9 +22,10 @@ function App() {
   return (
     <div className='bg-red-100'>
       <BrowserRouter>
+        <Navbar/>
         Aplicacion chicoti para encontrar y buscar perritos perdidos
         <AppRoutes/>
-
+        <Footer/>
       </BrowserRouter>
     </div>
   )
