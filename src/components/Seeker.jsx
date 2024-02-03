a336398bd4a14bad2831a06224aaf02a5abb8f7d
Ronald803 <ronaldblancobalboa@gmail.com>
2024-02-02 23:59:08 -0400

    four carousels added, each one for each gender and species

--- src/components/Seeker.jsx
+++ src/components/Seeker.jsx
@@ -9,14 +9,32 @@ function Seeker(props) {
     }
     const [allPets, setAllPets] = useState([])
     const [pets, setPets] = useState([]);
+    const [maleDogs, setMaleDogs] = useState([])
+    const [femaleDogs, setFemaleDogs] = useState([])
+    const [maleCats, setMaleCats] = useState([])
+    const [femaleCats, setFemaleCats] = useState([])
+
     useEffect(()=>{
         getSpecificPets()
     },[])
     const getSpecificPets = ()=>{
         getAnimalsBackend(characteristicPet)
             .then(ans=>{
+                let maleDogsAux=[];
+                let femaleDogsAux=[];
+                let maleCatsAux=[];
+                let femaleCatsAux=[];
                 setAllPets(ans.data.body)
-                setPets(ans.data.body);
+                ans.data.body.map(pet=>{
+                  if(pet.species == "dog" && pet.gender == "machito"){maleDogsAux.push(pet)}
+                  else if(pet.species == "dog" && pet.gender == "hembrita"){femaleDogsAux.push(pet)}
+                  else if(pet.species == "cat" && pet.gender == "machito"){maleCatsAux.push(pet)}
+                  else if(pet.species == "cat" && pet.gender == "hembrita"){femaleCatsAux.push(pet)}
+                })
+                setMaleDogs(maleDogsAux);
+                setFemaleDogs(femaleDogsAux);
+                setMaleCats(maleCatsAux);
+                setFemaleCats(femaleCatsAux);
             })
             .catch(e=>{console.log(e)})
     }
@@ -41,6 +59,26 @@ function Seeker(props) {
       <div>
         <form onSubmit={handleSubmit}>
           <div className='pt-2'>
+            <div><label className='ps-2' htmlFor='species'>Especie</label></div>
+            <input
+                type='species'
+                id='species'
+                name='species'
+                onChange={handleChange}
+                className='my-1 py-1 w-full rounded-lg border border-tertiary'
+            />
+          </div>
+          <div className='pt-2'>
+            <div><label className='ps-2' htmlFor='gender'>Género</label></div>
+            <input
+                type='gender'
+                id='gender'
+                name='gender'
+                onChange={handleChange}
+                className='my-1 py-1 w-full rounded-lg border border-tertiary'
+            />
+          </div>
+          <div className='pt-2'>
             <div><label className='ps-2' htmlFor='breed'>Raza</label></div>
             <input
                 type='breed'
@@ -54,9 +92,21 @@ function Seeker(props) {
             <button className='py-1 px-8 rounded-lg bg-primary text-white hover:bg-fourth hover:text-black'>Buscar</button>
           </div>
         </form>
+        <div>
+          <Carousel petsArray={pets} title='' link=''/>
+        </div>
       </div>
       <div>
-        <Carousel petsArray={pets} title='' link=''/>
+        <Carousel petsArray={maleDogs} title='Perritos' link=''/>
+      </div>
+      <div>
+        <Carousel petsArray={femaleDogs} title='Perritas' link=''/>
+      </div>
+      <div>
+        <Carousel petsArray={maleCats} title='Gatitos' link=''/>
+      </div>
+      <div>
+        <Carousel petsArray={femaleCats} title='Gatitas' link=''/>
       </div>
     </div>
   )
